f265f076667739dca01e9d659157ba9ab7af0944
Aiad Asaad <aiadasaadmesak@gmail.com>
2021-01-28 23:22:48 +0200

    Add files via upload

---
+++ Final_Project/Final.py
@@ -0,0 +1,153 @@
+import imutils
+from imutils import paths
+from imutils.video import VideoStream
+from imutils.video import FPS
+import face_recognition
+import pickle
+import time
+import cv2
+import os
+import csv
+from datetime import datetime
+import pandas as pd
+
+filename = "names.csv"
+header = ("Time_in", "Title")
+
+
+# initialize the list of known encodings and known names
+knownEncodings = []
+knownNames = []
+dataaa=[]
+times_s=[]
+times=[]
+
+
+print("[INFO] loading encodings + face detector...")
+data = pickle.loads(open('encodings.pickle', "rb").read())
+detector = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+# initialize the video stream and allow the camera sensor to warm up
+print("[INFO] starting video stream...")
+vs = VideoStream(src=0).start()
+#vs = VideoStream(usePiCamera=True).start()
+time.sleep(2.0)
+
+# start the FPS counter
+fps = FPS().start()
+
+# loop over frames from the video file stream
+while True:
+	# grab the frame from the threaded video stream and resize it
+	# to 500px (to speedup processing)
+	frame = vs.read()
+	frame = imutils.resize(frame, width=500)
+	
+	# convert the input frame from (1) BGR to grayscale (for face
+	# detection) and (2) from BGR to RGB (for face recognition)
+	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+	# detect faces in the grayscale frame
+	rects = detector.detectMultiScale(gray, scaleFactor=1.1, 
+		minNeighbors=5, minSize=(30, 30),
+		flags=cv2.CASCADE_SCALE_IMAGE)
+
+	# OpenCV returns bounding box coordinates in (x, y, w, h) order
+	# but we need them in (top, right, bottom, left) order, so we
+	# need to do a bit of reordering
+	boxes = [(y, x + w, y + h, x) for (x, y, w, h) in rects]
+
+	# compute the facial embeddings for each face bounding box
+	encodings = face_recognition.face_encodings(rgb, boxes)
+	names = []
+
+	# loop over the facial embeddings
+	for encoding in encodings:
+		# attempt to match each face in the input image to our known
+		# encodings
+		matches = face_recognition.compare_faces(data["encodings"],
+			encoding)
+		name = "Unknown"
+
+		# check to see if we have found a match
+		if True in matches:
+			# find the indexes of all matched faces then initialize a
+			# dictionary to count the total number of times each face
+			# was matched
+			matchedIdxs = [i for (i, b) in enumerate(matches) if b]
+			counts = {}
+
+			# loop over the matched indexes and maintain a count for
+			# each recognized face face
+			for i in matchedIdxs:
+				name = data["names"][i]
+				counts[name] = counts.get(name, 0) + 1
+
+			# determine the recognized face with the largest number
+			# of votes (note: in the event of an unlikely tie Python
+			# will select first entry in the dictionary)
+			name = max(counts, key=counts.get)
+			times = datetime.now()
+
+		
+		# update the list of names
+		names.append(name)
+                #times_s.append(times)
+  #       
+	# loop over the recognized faces
+	for ((top, right, bottom, left), name) in zip(boxes, names):
+		# draw the predicted face name on the image
+		cv2.rectangle(frame, (left, top), (right, bottom),
+			(0, 255, 0), 2)
+		y = top - 15 if top - 15 > 15 else top + 15
+		cv2.putText(frame, name, (left, y), cv2.FONT_HERSHEY_SIMPLEX,
+			0.75, (0, 255, 0), 2)
+
+	# display the image to our screen
+	cv2.imshow("Frame", frame)
+	key = cv2.waitKey(1) & 0xFF
+        
+	# if the `q` key was pressed, break from the loop
+	if key == ord("q"):
+                dataaa = [(times , names)]
+                break
+
+	# update the FPS counter
+	fps.update()
+
+#take the attendance
+
+
+
+
+
+# stop the timer and display FPS information
+fps.stop()
+print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
+print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
+
+with open (filename, "a", newline = "") as csvfile:
+        movies = csv.writer(csvfile)
+        #movies.writerow(header)
+        for x in dataaa:
+                movies.writerow(x)   
+
+	
+final=pd.read_csv("names.csv")
+#Drop all duplicates from the dataset
+final.drop_duplicates(subset='Title', keep="last",inplace=True)
+
+a=list(final.query(""""['Unknown']" in Title""").index)
+b=list(final.query(""""[]" in Title""").index)
+final.drop(a,axis=0,inplace=True)
+final.drop(b,axis=0,inplace=True)
+
+
+final.to_csv (r'attendence.csv', index = False, header=True)
+
+
+
+# do a bit of cleanup
+cv2.destroyAllWindows()
+vs.stop()
